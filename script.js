23811fd40f1c42787cde80346a87ad43900b6c62
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-10-23 11:56:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+const fontSizeInput=document.getElementById("fontsize");
+const fontColorInput=document.getElementById("fontcolor");
+const buttonElement=document.getElementById("button");
+
+buttonElement.addEventListener("click",function(){
+	event.preventDefault();
+	const fontSizeValue=fontSizeInput.value;
+	const fontColorValue=fontColorInput.value;
+	document.cookie(`fontsize=${fontSizeValue}px`)
+	document.cookie(`fontcolor=${fontColorValue}`)
+	 document.documentElement.style.setProperty("--fontsize", fontSizeValue + "px");
+  document.documentElement.style.setProperty("--fontcolor", fontColorValue);
+
+	})
+window.addEventListener("load", () => {
+  const savedFontSize = getCookie("fontsize");
+  const savedFontColor = getCookie("fontcolor");
+
+  if (savedFontSize) {
+    document.documentElement.style.setProperty("--fontsize", savedFontSize);
+    fontSizeInput.value = parseInt(savedFontSize); // remove px for input
+  }
+  if (savedFontColor) {
+    document.documentElement.style.setProperty("--fontcolor", savedFontColor);
+    fontColorInput.value = savedFontColor;
+  }
+});
